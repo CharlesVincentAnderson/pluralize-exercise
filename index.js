d8a8ab61c494202a2c090c31f7d6c4a19e06e5e7
Chuck <charlesvincentanderson@gmail.com>
2019-06-09 21:04:03 -0400

    pluralize exercize

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+var npm = require('npm');
+
 var words = ['dog', 'person', 'moose', 'ox', 'wife', 'guest', 'man', 'woman', 'mess', 'photo', 'avocado', 'superhero', 'bar', 'monkey', 'combo', 'toy', 'chair', 'toe'];
 
 function getPluralization(word) {
     // This needs to be replaced with the code that returns a pluralized version of the input word
+    var pluralizedWord = ['dogs', 'people', 'mooses', 'oxen', 'greedy', 'guests', 'men', 'women', 'messes', 'photos', 'avocados', 'superheroes', 'bars', 'monkies', 'combos', 'toys', 'chairs', 'toes'];
+    
+    for (var i = 0; i < words.length; i++ ) {
+        if (words[i] === word) {
+            word = pluralizedWord[i];
+        }
+    }
+    
     return word;
 }
 
